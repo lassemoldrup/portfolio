b19b12abb962c3574b51fc7b87fef96d57dafe38
crahs8 <lasse.moeldrup@gmail.com>
2021-07-28 23:55:06 +0200

    refactor tiles

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import About from './components/About.svelte';
+    import Tile from './components/Tile.svelte';
     import ChessIcon from './icons/chess.svg';
     import LambdaIcon from './icons/lambda.svg';
+
+
 </script>
 
 
@@ -18,121 +21,33 @@
             grid-template: 33vw auto / 1fr 1fr 1fr;
         }
     }
-
-    .tile {
-        padding: 5%;
-        display: flex;
-        flex-direction: column;
-        overflow: hidden;
-        transition: background-color 80ms cubic-bezier(0, 0, 0.5, 1);
-        font-size: 1.2rem;
-
-        p {
-            margin: 0;
-        }
-
-        h2 {
-            text-align: center;
-            margin-bottom: calc(10% - 0.5rem);
-            margin-top: 0;
-            line-height: 1em;
-        }
-
-        :global(.icon) {
-            width: 100%;
-            max-height: 100%;
-            flex-grow: 1;
-            fill: var(--lighter);
-            transition: max-height 150ms cubic-bezier(0, 0, 0.5, 1), opacity 150ms cubic-bezier(0, 0, 0.5, 1);
-            transform: scale(0.5);
-        }
-
-        &:hover, &:focus {
-            cursor: pointer;
-
-            :global(.icon) {
-                max-height: 0;
-                opacity: 0;
-            }
-
-            .description {
-                opacity: 100;
-            }
-        }
-    }
-
-    .description {
-        opacity: 0;
-        height: 0;
-        transition: opacity 150ms cubic-bezier(0, 0, 0.5, 1);
-    }
-
-    #chess-tile {
-        //background-color: hsl(268, 15%, 30%);
-        background-color: hsl(203, 15%, 30%);
-
-        &:hover, &:focus {
-            //background-color: hsl(268, 15%, 15%);
-            background-color: hsl(203, 15%, 15%);
-        }
-    }
-
-    #lambda-tile {
-        //background-color: hsl(140, 15%, 30%);
-        background-color: hsl(347, 15%, 30%);
-
-        &:hover, &:focus {
-            background-color: hsl(347, 15%, 15%);
-        }
-    }
-
-    #sat-tile {
-        background-color: hsl(55, 15%, 30%);
-
-        &:hover, &:focus {
-            background-color: hsl(55, 15%, 15%);
-        }
-    }
 </style>
 
 
 <div id="grid">
-    <div id="chess-tile" class="tile">
-        <h2>Chess engine</h2>
+    <Tile header="Chess engine"
+        description="A UCI-compatible chess engine written in Rust."
+        color="hsl(203, 15%, 30%)"
+        hoverColor="hsl(203, 15%, 15%)"
+        techs="{['Rust', 'WebAssembly']}">
         <ChessIcon class="icon"/>
-        <div class="description">
-            <p>A UCI-compatible chess engine written in Rust.</p>
-            <h3>Tech</h3>
-            <ul>
-                <li>Rust</li>
-                <li>WebAssembly</li>
-            </ul>
-        </div>
-    </div>
-    <div id="lambda-tile" class="tile">
-        <h2>Lambda calculus</h2>
+    </Tile>
+
+    <Tile header="Lambda calculus"
+        description="A lambda calculus interpretter written in Rust.
+            It supports macros along with different evaluation orders."
+        color="hsl(347, 15%, 30%)"
+        hoverColor="hsl(347, 15%, 15%)"
+        techs="{['Rust', 'WebAssembly']}">
         <LambdaIcon class="icon"/>
-        <div class="description">
-            <p>A lambda calculus interpretter written in Rust.
-            It supports macros along with different evaluation orders.</p>
-            <h3>Tech</h3>
-            <ul>
-                <li>Rust</li>
-                <li>WebAssembly</li>
-            </ul>
-        </div>
-    </div>
-    <div id="sat-tile" class="tile">
-        <h2>Sat solver</h2>
-        <ChessIcon class="icon"/>
-        <div class="description">
-            <p>A SAT-solver written in Rust.</p>
-            <h3>Tech</h3>
-            <ul>
-                <li>Rust</li>
-                <li>WebAssembly</li>
-            </ul>
-        </div>
-    </div>
+    </Tile>
+
+    <Tile header="Sat solver"
+        description="A SAT solver written in Rust."
+        color="hsl(55, 15%, 30%)"
+        hoverColor="hsl(55, 15%, 15%)"
+        techs="{['Rust', 'WebAssembly']}">
+        <LambdaIcon class="icon"/>
+    </Tile>
 </div>
 <About/>
---
+++ src/components/Tile.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    export let header: string;
+    export let description: string;
+    export let color: string;
+    export let hoverColor: string;
+    export let techs: string[];
+</script>
+
+
+<style lang="scss">
+    .tile {
+        padding: 5%;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        transition: background-color 80ms cubic-bezier(0, 0, 0.5, 1);
+        font-size: 1.2rem;
+        background-color: var(--bg-color);
+
+        p {
+            margin: 0;
+        }
+
+        h2 {
+            text-align: center;
+            margin-bottom: calc(10% - 0.5rem);
+            margin-top: 0;
+            line-height: 1em;
+        }
+
+        :global(.icon) {
+            width: 100%;
+            max-height: 100%;
+            flex-grow: 1;
+            fill: var(--lighter);
+            transition: max-height 150ms cubic-bezier(0, 0, 0.5, 1), opacity 150ms cubic-bezier(0, 0, 0.5, 1);
+            transform: scale(0.5);
+        }
+
+        &:hover, &:focus {
+            background-color: var(--hover-color);
+            cursor: pointer;
+
+            :global(.icon) {
+                max-height: 0;
+                opacity: 0;
+            }
+
+            .description {
+                opacity: 100;
+            }
+        }
+    }
+
+    .description {
+        opacity: 0;
+        height: 0;
+        transition: opacity 150ms cubic-bezier(0, 0, 0.5, 1);
+    }
+</style>
+
+
+<div class="tile" style="--bg-color: {color}; --hover-color: {hoverColor}">
+    <h2>{header}</h2>
+    <slot/>
+    <div class="description">
+        <p>{description}</p>
+        <h3>Tech</h3>
+        <ul>
+            {#each techs as tech}
+            <li>{tech}</li>
+            {/each}
+        </ul>
+    </div>
+</div>
